535829cea49e15b4f9f5c5e093fa7b0a3b75ff83
sentiox <trooxrapiex@gmail.com>
2025-12-16 10:36:39 +0300

    Update nft.sh

--- sentinel/files/usr/lib/nft.sh
+++ sentinel/files/usr/lib/nft.sh
@@ -1,30 +1,83 @@
-# Create an nftables table in the inet family
 nft_create_table() {
     local name="$1"
-
-    nft add table inet "$name"
+    nft add table inet "$name" 2>/dev/null
 }
 
-# Create a set within a table for storing IPv4 addresses
 nft_create_ipv4_set() {
     local table="$1"
     local name="$2"
 
-    nft add set inet "$table" "$name" '{ type ipv4_addr; flags interval; auto-merge; }'
+    nft add set inet "$table" "$name" \
+        '{ type ipv4_addr; flags interval; auto-merge; }' 2>/dev/null
 }
 
 nft_create_ifname_set() {
     local table="$1"
     local name="$2"
 
-    nft add set inet "$table" "$name" '{ type ifname; flags interval; }'
+    nft add set inet "$table" "$name" \
+        '{ type ifname; flags interval; }' 2>/dev/null
 }
 
-# Add one or more elements to a set
 nft_add_set_elements() {
     local table="$1"
     local set="$2"
     local elements="$3"
 
-    nft add element inet "$table" "$set" "{ $elements }"
+    [ -z "$elements" ] && return 0
+
+    nft add element inet "$table" "$set" "{ $elements }" 2>/dev/null
+}
+
+nft_create_mangle_chain() {
+    local table="$1"
+
+    nft add chain inet "$table" mangle \
+        '{ type filter hook prerouting priority mangle; policy accept; }' 2>/dev/null
+}
+
+nft_create_mangle_output_chain() {
+    local table="$1"
+
+    nft add chain inet "$table" mangle_output \
+        '{ type route hook output priority mangle; policy accept; }' 2>/dev/null
+}
+
+nft_create_proxy_chain() {
+    local table="$1"
+
+    nft add chain inet "$table" proxy \
+        '{ type filter hook prerouting priority dstnat; policy accept; }' 2>/dev/null
+}
+
+nft_add_common_subnet_rules() {
+    local table="$1"
+
+    nft add rule inet "$table" mangle \
+        iifname "@$NFT_INTERFACE_SET_NAME" ip daddr "@$NFT_COMMON_SET_NAME" \
+        meta l4proto tcp meta mark set 0x105 counter 2>/dev/null
+
+    nft add rule inet "$table" mangle \
+        iifname "@$NFT_INTERFACE_SET_NAME" ip daddr "@$NFT_COMMON_SET_NAME" \
+        meta l4proto udp meta mark set 0x105 counter 2>/dev/null
+}
+
+nft_add_roblox_rules() {
+    local table="$1"
+
+    nft_create_ipv4_set "$table" "$NFT_ROBLOX_SET_NAME"
+
+    nft add rule inet "$table" mangle \
+        iifname "@$NFT_INTERFACE_SET_NAME" ip daddr "@$NFT_ROBLOX_SET_NAME" \
+        meta mark set 0x105 counter 2>/dev/null
+}
+
+nft_add_discord_rules() {
+    local table="$1"
+
+    nft_create_ipv4_set "$table" "$NFT_DISCORD_SET_NAME"
+
+    nft add rule inet "$table" mangle \
+        iifname "@$NFT_INTERFACE_SET_NAME" ip daddr "@$NFT_DISCORD_SET_NAME" \
+        udp dport '{ 50000-65535 }' meta mark set 0x105 counter 2>/dev/null
 }
